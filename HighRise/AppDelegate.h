44296d09c7117d5ddfdfe19b38a1c7773ed6dfe8
liyu <13798464518@163.com>
2018-03-21 16:06:14 +0800

    Initial Commit

---
+++ HighRise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighRise
+//
+//  Created by liyu on 2018/3/21.
+//  Copyright © 2018年 liyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HighRise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HighRise
+//
+//  Created by liyu on 2018/3/21.
+//  Copyright © 2018年 liyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
